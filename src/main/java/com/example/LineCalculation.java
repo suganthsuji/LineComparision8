f94762261728382ec265e60b0206e8f7db2dc6e4
suganthsuji <suganthpv.007@gmail.com>
2022-07-18 22:20:28 +0530

    Revert "Added Usecase3"
    This reverts commit f8d053bb4cb70207c11e4695ab9e56ed42b7596d.

--- src/main/java/com/example/LineCalculation.java
+++ src/main/java/com/example/LineCalculation.java
@@ -1,23 +1,13 @@
 package com.example;
 
-class LineCalculation
-{
+public class LineCalculation {
     private double x,y;
-
-    /*
-    A Constructor to assign two coordinates to x and y
-     */
     LineCalculation(double x1, double y1) {
         x = x1;
         y = y1;
     }
-    /*
-    Function Distance with Return type Double is used find the distance of two points
-     */
-    double distance(LineCalculation p)
-    {
+    double distance(LineCalculation p) {
         return Math.sqrt( Math.pow(p.x - x,2) + Math.pow(p.y -y,2) );
     }
+
 }
-
-
