edcdb485b4bd8937664fd7ac052cd354b5f76f1e
suganthsuji <suganthpv.007@gmail.com>
2022-07-18 22:26:26 +0530

    Added: uc3

--- src/main/java/com/example/LineCalculation.java
+++ src/main/java/com/example/LineCalculation.java
@@ -2,11 +2,13 @@ package com.example;
 
 public class LineCalculation {
     private double x,y;
+
     LineCalculation(double x1, double y1) {
         x = x1;
         y = y1;
     }
-    double distance(LineCalculation p) {
+    double distance(LineCalculation p)
+    {
         return Math.sqrt( Math.pow(p.x - x,2) + Math.pow(p.y -y,2) );
     }
 
